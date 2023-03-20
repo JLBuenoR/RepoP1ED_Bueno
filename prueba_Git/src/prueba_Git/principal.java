22b2084fc585820d27187ff50df15c0164577cb2
Jose <Jose@172.16.3.239>
2023-03-20 17:22:05 +0000

    prueba

---
+++ prueba_Git/src/prueba_Git/principal.java
@@ -0,0 +1,12 @@
+package prueba_Git;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Prueba de Git para entornos de "
+				+ "Desarrollo.");
+
+	}
+
+}
